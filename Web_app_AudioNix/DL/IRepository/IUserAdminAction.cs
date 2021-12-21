205fbd30e04676966e4e20c393414a8ac88e5b24
Kadgarr <55097790+Kadgarr@users.noreply.github.com>
2021-12-21 18:44:39 +0200

    Add two interfaces

---
+++ Web_app_AudioNix/DL/IRepository/IUserAdminAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DL.IRepository
+{
+    interface IUserAdminAction
+    {
+        public void Download();
+        public void View();
+        public void Search();
+        public void AddSongToPlaylist();
+        public void DeleteSongFromPlaylist();
+        public void Registration();
+        public void Auntification();
+        public void LookProfile();
+
+    }
+}
