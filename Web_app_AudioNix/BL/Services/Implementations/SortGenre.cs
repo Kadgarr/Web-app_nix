b474f97322a68c29eaa518c72beb39044f83c6e6
Kadgarr <55097790+Kadgarr@users.noreply.github.com>
2022-01-08 15:53:06 +0200

    ApplicationContext
    -added ApplicationContext class

--- Web_app_AudioNix/BL/Services/Implementations/SortGenre.cs
+++ Web_app_AudioNix/BL/Services/Implementations/SortGenre.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 using BL.Services.Interfaces;
-
+using BL.DtoEntities;
 namespace BL.Services.Implementations
 {
-    public class SortGenre
+    public class SortGenre : ISort<GenreDTO>
     {
+        public IEnumerable<GenreDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
 
+        public IEnumerable<GenreDTO> SearchByItem(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GenreDTO> SortByDesc()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GenreDTO> SortByInc()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Web_app_AudioNix/DL/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DL.Entities;
+
+namespace DL
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Genres_of_Music> Genres_of_Music { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Playlist_of_User> Playlist_of_User { get; set; }
+        public DbSet<Singer> Singers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WebAppNix_DB;Trusted_Connection=True;");
+        }
+    }
+}
