d0474d74615752a0ba40b3de93c4d0d7b748dcd3
Kadgarr <55097790+Kadgarr@users.noreply.github.com>
2022-06-30 15:43:17 +0300

    Added realization

---
+++ Web_app_AudioNix/WebApplication_API/Controllers/JournalController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JournalController : ControllerBase
+    {
+        static List<Journal> _journals = new List<Journal>
+        {
+            new Journal( Guid.NewGuid(), "Name1", new DateTime(2015, 7, 20), true),
+            new Journal( Guid.NewGuid(), "Name2", new DateTime(2015, 7, 20), false),
+            new Journal( Guid.NewGuid(), "Name3", new DateTime(2015, 7, 20), false),
+            new Journal( Guid.NewGuid(), "Name4", new DateTime(2015, 7, 20), true),
+            new Journal( Guid.NewGuid(), "Name5", new DateTime(2015, 7, 20), false)
+        };
+
+
+        private readonly ILogger<JournalController> _logger;
+
+        public JournalController(ILogger<JournalController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Journal>> Get()
+        {
+            return _journals.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Journal> Get(Guid id)
+        {
+            var user =  _journals.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+            return new ObjectResult(user);
+        }
+
+        [HttpPost]
+        public ActionResult<Journal> Post(Journal journal)
+        {
+            if (journal == null)
+            {
+                return BadRequest();
+            }
+
+            _journals.Add(journal);
+            
+            return Ok(_journals);
+        }
+
+        [HttpPut]
+        public ActionResult<Journal> Patch(Journal journal)
+        {
+            if (journal == null)
+            {
+                return BadRequest();
+            }
+            if (!_journals.Any(x => x.Id == journal.Id))
+            {
+                return NotFound();
+            }
+
+            var index = _journals.IndexOf(_journals.Where(x => x.Id == journal.Id).FirstOrDefault());
+
+            _journals[index]=journal;
+
+            
+            return Ok(journal);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Journal> Delete(Guid id)
+        {
+            var journal = _journals.FirstOrDefault(x => x.Id == id);
+            if (journal == null)
+                return NotFound();
+            _journals.Remove(journal);
+            return Ok(journal);
+        }
+    }
+}
+
---
+++ Web_app_AudioNix/WebApplication_API/Journal.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication_API
+{
+    public class Journal
+    {
+        
+        public Journal(Guid id, string fullName, DateTime date, bool attendance)
+        {
+            Id = id;
+            FullName = fullName;
+            Date = date;
+            Attendance = attendance;
+        }
+
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime Date { get; set; }
+        public bool Attendance { get; set; } 
+    }
+}
